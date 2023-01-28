fb5d34cb487539a869cc51e78d4dd4d7b3aed1c0
Zac Long <zac.long.96@gmail.com>
2023-01-28 14:38:04 +1100

    updated db connection and env

--- db.js
+++ db.js
@@ -4,3 +4,16 @@ dotenv.config()
 
 mongoose.set('strictQuery', true)
 
+async function dbClose() {
+    await mongoose.connection.close()
+    console.log('Connection closed')
+}
+
+try {
+    const db = await mongoose.connect(process.env.MONGO_URL)
+    console.log(db.connection.readyState === 1 ? 'Connected to DB' : 'Not connected to DB')
+}
+catch (err) {
+    console.log(err)
+}
+
