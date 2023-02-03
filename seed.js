94188e72588eb8fcf991707a9af4455c50f63864
Zac Long <zac.long.96@gmail.com>
2023-02-03 12:33:14 +1100

    updated seed to match form submisison

--- seed.js
+++ seed.js
@@ -20,16 +20,17 @@ const sta = await StateModel.insertMany(states)
 console.log('States inserted')
 
 const courts = [
-    { address: '123 Hoop St', city: 'Bondi', state: sta[1], description: 'Sydney Court, hoop in good condition, no net' },
-    { address: '456 Basket St', city: 'Abbotsford', state: sta[6], description: 'asphalt, backboard broken' }
+    { name: 'Hoop St Court',  address: '123 Hoop St', city: 'Bondi', state: sta[1], description: 'Sydney Court, hoop in good condition, no net' },
+    { name: 'Quentins court', address: '456 Basket St', city: 'Abbotsford', state: sta[6], description: 'asphalt, backboard broken' },
+    { name: 'Sportscenter Court', address: '23 City St', city: 'Hobart', state: sta[5] }
 ]
 
 await CourtModel.insertMany(courts)
 console.log('Courts inserted')
 
 const games = [
-    { address: '123 Hoop St', state: sta[1], time: '12:00', date: '2021-01-01', skillLevel: 'beginner', description: 'first game' },
-    { address: '456 Basket St', state: sta[6], time: '18:00', date: '2021-04-01', skillLevel: 'highly-skilled', description: 'regular scheduled game' }
+    { title: 'Anyones game!', address: '123 Hoop St', city: 'Bondi', state: sta[1], time: '12:00', date: '2021-01-01', skillLevel: "Anyone's welcome", description: 'first game' },
+    { title: "Matt's Game", address: '456 Basket St', city: 'Abbotsford', state: sta[6], time: '18:00', date: '2021-04-01', skillLevel: 'Intermediate', description: 'regular scheduled game' }
 ]
 
 await GameModel.insertMany(games)
